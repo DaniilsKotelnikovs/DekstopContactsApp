2e6bdce5eb648b0ef58eff295615d182b4806923
Daniils Kotelnikovs <ziekor11@gmail.com>
2023-07-18 13:47:06 +0300

    Custom contact control usage.
    Implemented usage of previously created custom contact control.

--- DesktopContactsApp/Controls/ContactControl.xaml.cs
+++ DesktopContactsApp/Controls/ContactControl.xaml.cs
@@ -19,20 +19,26 @@ namespace DesktopContactsApp.Controls
     /// </summary>
     public partial class ContactControl : UserControl
     {
-        private Contact contact;
-
         public Contact Contact
         {
-            get { return contact; }
-            set
-            {
-                contact = value;
-                nameTextBlock.Text = contact.Name;
-                phoneTextBlock.Text = contact.Phone;
-                emailTextBlock.Text = contact.Email;
-            }
+            get { return (Contact)GetValue(ContactProperty); }
+            set { SetValue(ContactProperty, value); }
         }
 
+        // Using a DependencyProperty as the backing store for Contact.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContactProperty =
+            DependencyProperty.Register("Contact", typeof(Contact), typeof(ContactControl), new PropertyMetadata(new Contact()
+            {Name ="Name Lastname", Phone = "(123) 4232 6366", Email = "example@domain.com"}, SetText));
+
+        private static void SetText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ContactControl control = d as ContactControl;
+            if (control == null) return;
+
+            control.nameTextBlock.Text = (e.NewValue as Contact).Name;
+            control.phoneTextBlock.Text = (e.NewValue as Contact).Phone;
+            control.emailTextBlock.Text = (e.NewValue as Contact).Email;
+        }
 
         public ContactControl()
         {
